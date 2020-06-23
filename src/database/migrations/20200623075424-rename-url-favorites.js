7210618d59315f6c69775445dd8313c7705b754f
Marcos Oliveira <marcosoliveirajunior@MacBook-Pro-de-Marcos.local>
2020-06-23 05:37:30 -0300

    return of api fixed

--- src/database/migrations/20200623075424-rename-url-favorites.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  up: (queryInterface) => {
-    return queryInterface.renameColumn('favorites', 'url', 'image_url');
-  },
-
-  down: (queryInterface) => {
-    return queryInterface.renameColumn('favorites', 'image_url', 'url');
-  },
-};
